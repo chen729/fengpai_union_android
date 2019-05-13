2830b35f608d2e70908b238eb0270c252da2e417
chen729 <50562666+chen729@users.noreply.github.com>
2019-05-13 11:13:04 +0800

    测试

---
+++ app/src/main/java/com/example/alliance/MyApplication.java
@@ -0,0 +1,68 @@
+package com.example.alliance;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+/**
+ * 全局公共类，全局变量
+ */
+public class MyApplication extends Application {
+
+
+    private int count = 0;   ////记录当前的窗口数量
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+            count ++;
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+            if(count > 0) {
+                count--;
+            }
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+        }
+    });
+}
+
+
+    /**
+     * 判断app是否在后台
+     * @return
+     */
+    public boolean isBackground(){
+        if(count <= 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
